d4b57f2314ecc147bbb70bc26f3e3eaf20c40415
benjamin-korobkin <benjamin.korobkin@gmail.com>
2025-12-04 22:05:27 +0200

    changes plus commiting other files

--- src/new_card_game/cards/WarriorFront.gd
+++ src/new_card_game/cards/WarriorFront.gd
@@ -2,10 +2,10 @@ extends "res://src/new_card_game/CardFront.gd"
 
 
 func _ready() -> void:
-	card_labels["Era"] = find_node("Era")
-	card_label_min_sizes["Era"] = Vector2(CFConst.CARD_SIZE.x - 4, STANDARD_FONT_SIZE)
-	card_labels["Teaching"] = find_node("Teaching")
-	card_label_min_sizes["Teaching"] = Vector2(CFConst.CARD_SIZE.x - 4, STANDARD_FONT_SIZE + 2)
+	card_labels["Type"] = find_node("Type")
+	card_label_min_sizes["Type"] = Vector2(CFConst.CARD_SIZE.x - 4, STANDARD_FONT_SIZE)
+	card_labels["Name"] = find_node("Name")
+	card_label_min_sizes["Name"] = Vector2(CFConst.CARD_SIZE.x - 4, STANDARD_FONT_SIZE + 2)
 	card_labels["Power"] = find_node("Power")
 	card_label_min_sizes["Power"] = Vector2(CFConst.CARD_SIZE.x - 4,STANDARD_FONT_SIZE)
 	# Repeat these methods to get in the card_labels.rect_min_size
